6d26de6c47110012c02d5576c31c00947cebc6cc
mici <60635066+midorimici@users.noreply.github.com>
2022-01-11 13:56:00 +0900

    🔧 Setup Chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
 
 export default MyApp;
