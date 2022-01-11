33cf58f10f344dd9b60498155386438438c56c7f
mici <60635066+midorimici@users.noreply.github.com>
2022-01-11 14:19:39 +0900

    ✨ Add `Footer` component

---
+++ src/components/atoms/Footer/Footer.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Footer } from '.';
+
+const Meta: ComponentMeta<typeof Footer> = {
+  title: 'Atoms/Footer',
+  component: Footer,
+};
+
+export default Meta;
+
+const Template: ComponentStory<typeof Footer> = (args) => <Footer {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: <>&copy; 2022 Midori Mici</>,
+};
