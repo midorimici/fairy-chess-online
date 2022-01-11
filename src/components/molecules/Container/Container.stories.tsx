70f08eb96182571f7997974f8d586205a27d0052
mici <60635066+midorimici@users.noreply.github.com>
2022-01-11 15:46:32 +0900

    ✨ Add `Container` component

---
+++ src/components/molecules/Container/Container.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Container } from '.';
+
+const Meta: ComponentMeta<typeof Container> = {
+  title: 'Molecules/Container',
+  component: Container,
+};
+
+export default Meta;
+
+const Template: ComponentStory<typeof Container> = (args) => <Container {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: <p>Main contents</p>,
+};
