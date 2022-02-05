d2a1212e949117e97abc7e63506dd249c01325e5
mici <60635066+midorimici@users.noreply.github.com>
2022-02-05 18:45:35 +0900

    ✨ Fetch games data from GraphCMS

---
+++ src/pages/games.tsx
@@ -0,0 +1,42 @@
+import type { GetStaticProps, NextPage } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import type { Namespace } from 'react-i18next';
+import { fetchGql } from '~/lib/graphcms';
+
+type Props = {
+  games: Game[];
+};
+
+const GamesPage: NextPage<Props> = ({ games }) => (
+  <ul>
+    {games.map((game: Game) => (
+      <div key={game.id}>
+        <li>{game.name}</li>
+        <li>{game.description}</li>
+      </div>
+    ))}
+  </ul>
+);
+
+export default GamesPage;
+
+const namespaces: Namespace = ['common'];
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  const data = await fetchGql<GamesResponse>(`
+    {
+      games {
+        id
+        name
+        description
+      }
+    }
+  `);
+
+  return {
+    props: {
+      games: data.games,
+      ...(await serverSideTranslations(locale ?? 'en', namespaces)),
+    },
+  };
+};
