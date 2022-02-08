e598b4a1f135897bdb97b15ae484a05338e469ba
mici <60635066+midorimici@users.noreply.github.com>
2022-02-08 16:18:19 +0900

    ✨ Add `GameList` component

---
+++ src/components/molecules/GameList/GameList.tsx
@@ -0,0 +1,14 @@
+import { Stack } from '@chakra-ui/react';
+import { GameListItem } from '~/components/atoms';
+
+type Props = {
+  games: Game[];
+};
+
+export const GameList: React.FC<Props> = ({ games }) => (
+  <Stack spacing={8}>
+    {games.map((game: Game) => (
+      <GameListItem key={game.id} game={game} />
+    ))}
+  </Stack>
+);
