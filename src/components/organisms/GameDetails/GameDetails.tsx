541515fa8eebdbade474e2ae6144a5d1fa5fd5fa
mici <60635066+midorimici@users.noreply.github.com>
2022-05-05 23:01:49 +0900

    ✨ Add `GameDetails` component

---
+++ src/components/organisms/GameDetails/GameDetails.tsx
@@ -0,0 +1,19 @@
+import { Text, VStack } from '@chakra-ui/react';
+import Image from 'next/image';
+
+type Props = {
+  game: Game;
+};
+
+export const GameDetails: React.FC<Props> = ({ game }) => (
+  <VStack alignItems="flex-start" gap={2} flexGrow={1}>
+    <Image
+      src={game.initialBoardImage.url}
+      alt={game.name}
+      width={game.initialBoardImage.width}
+      height={game.initialBoardImage.height}
+    />
+    <Text fontSize="2xl">{game.name}</Text>
+    <Text>{game.description}</Text>
+  </VStack>
+);
