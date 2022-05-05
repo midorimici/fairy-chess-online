9c1d9004639100c99b7c6c0c3519d9e6eb8e4a77
mici <60635066+midorimici@users.noreply.github.com>
2022-05-05 23:26:03 +0900

    🌐 i18n

--- src/components/templates/Game/Game.tsx
+++ src/components/templates/Game/Game.tsx
@@ -1,6 +1,7 @@
 import { Button, HStack, VStack } from '@chakra-ui/react';
 import Language from '@mui/icons-material/Language';
 import Lock from '@mui/icons-material/Lock';
+import { useTranslation } from 'next-i18next';
 import { BreadcrumbNav, Container } from '~/components/molecules';
 import { GameDetails } from '~/components/organisms';
 
@@ -8,32 +9,36 @@ type Props = {
   game: Game;
 };
 
-export const Game: React.FC<Props> = ({ game }) => (
-  <Container>
-    <VStack alignItems="stretch" spacing={4}>
-      <BreadcrumbNav
-        items={[
-          {
-            name: 'Games',
-            path: '/games',
-          },
-          {
-            name: game.name,
-            path: '',
-          },
-        ]}
-      />
-      <HStack alignItems="flex-start">
-        <GameDetails game={game} />
-        <VStack alignItems="stretch" spacing={4}>
-          <Button size="lg" leftIcon={<Language />}>
-            Play in a public room
-          </Button>
-          <Button size="lg" leftIcon={<Lock />}>
-            Play in a private room
-          </Button>
-        </VStack>
-      </HStack>
-    </VStack>
-  </Container>
-);
+export const Game: React.FC<Props> = ({ game }) => {
+  const { t } = useTranslation();
+
+  return (
+    <Container>
+      <VStack alignItems="stretch" spacing={4}>
+        <BreadcrumbNav
+          items={[
+            {
+              name: t('games'),
+              path: '/games',
+            },
+            {
+              name: game.name,
+              path: '',
+            },
+          ]}
+        />
+        <HStack alignItems="flex-start">
+          <GameDetails game={game} />
+          <VStack alignItems="stretch" spacing={4}>
+            <Button size="lg" leftIcon={<Language />}>
+              {t('play-public')}
+            </Button>
+            <Button size="lg" leftIcon={<Lock />}>
+              {t('play-private')}
+            </Button>
+          </VStack>
+        </HStack>
+      </VStack>
+    </Container>
+  );
+};
